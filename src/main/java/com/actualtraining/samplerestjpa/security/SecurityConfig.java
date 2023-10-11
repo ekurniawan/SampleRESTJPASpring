f234eab8c862bed656b5ad7ae10f2b97707297eb
erickkurniawan <erick.kurniawan@gmail.com>
2023-10-11 16:03:39 +0700

    menambahkan swagger dan jwt token

--- src/main/java/com/actualtraining/samplerestjpa/security/SecurityConfig.java
+++ src/main/java/com/actualtraining/samplerestjpa/security/SecurityConfig.java
@@ -42,7 +42,16 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/auth/**","/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        // -- Swagger UI v3 (OpenAPI)
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
